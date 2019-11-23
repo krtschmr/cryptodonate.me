71d28baa8fa574b29f11f98a7457f6d40527733c
Tim Kretschmer <tim@krtschmr.de>
2019-11-23 16:20:01 +0000

    metrica scss

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_SECRET"], callback_url: "/users/auth/twitch/callback"
   provider :mixer, ENV["MIXER_CLIENT_ID"], ENV["MIXER_SECRET"], callback_url: "/users/auth/mixer/callback"
 
-  # provider :streamelements, ENV["STREAMELEMENTS_CLIENT_ID"], ENV["STREAMELEMENTS_SECRET"], callback_url: "/users/auth/streamelements/callback"
-  # provider :streamlabs, ENV["STREAMLABS_CLIENT_ID"], ENV["STREAMLABS_SECRET"], callback_url: "/users/auth/streamlabs/callback"
+  provider :streamelements, ENV["STREAMELEMENTS_CLIENT_ID"], ENV["STREAMELEMENTS_SECRET"], callback_url: "/users/auth/streamelements/callback"
+  provider :streamlabs, ENV["STREAMLABS_CLIENT_ID"], ENV["STREAMLABS_SECRET"], callback_url: "/users/auth/streamlabs/callback"
 end
