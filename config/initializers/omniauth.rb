01adde7fa75f607f7e781325fdf7c6a739f625c3
Tim Kretschmer <tim@krtschmr.de>
2019-11-25 05:58:15 +0000

    force verify

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_SECRET"] #, callback_path: "/streamers/auth/twitch/callback"
-  provider :mixer, ENV["MIXER_CLIENT_ID"], ENV["MIXER_SECRET"] #, callback_path: "/streamers/auth/mixer/callback"
+  provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_SECRET"], authorize_params: {force_verify: true}
+  provider :mixer, ENV["MIXER_CLIENT_ID"], ENV["MIXER_SECRET"], authorize_params: {force_verify: true}
 
-  provider :streamelements, ENV["STREAMELEMENTS_CLIENT_ID"], ENV["STREAMELEMENTS_SECRET"] #, callback_path: "/streamer/auth/twitch/callback"
+  provider :streamelements, ENV["STREAMELEMENTS_CLIENT_ID"], ENV["STREAMELEMENTS_SECRET"]
   provider :streamlabs, ENV["STREAMLABS_CLIENT_ID"], ENV["STREAMLABS_SECRET"], scope: 'donations.create'
 end
